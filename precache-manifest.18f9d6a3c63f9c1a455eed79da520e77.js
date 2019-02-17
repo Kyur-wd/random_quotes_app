5ed0978f44723f2b6f6ad41ac32c882195980b3b
Bilal Ahmad <kyuranx@outlook.de>
2019-02-17 11:33:30 +0100

    Updates

---
+++ precache-manifest.18f9d6a3c63f9c1a455eed79da520e77.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d70776cc3486e64d112f",
+    "url": "/random_quotes_app/static/css/main.cdb18e8c.chunk.css"
+  },
+  {
+    "revision": "d70776cc3486e64d112f",
+    "url": "/random_quotes_app/static/js/main.d70776cc.chunk.js"
+  },
+  {
+    "revision": "fbfa9ac1c04d6f818c50",
+    "url": "/random_quotes_app/static/js/1.fbfa9ac1.chunk.js"
+  },
+  {
+    "revision": "e60c39e55d9d1f7880ad",
+    "url": "/random_quotes_app/static/js/runtime~main.e60c39e5.js"
+  },
+  {
+    "revision": "c5ecbc30dd20147a2b8a9c1f46cc86e2",
+    "url": "/random_quotes_app/index.html"
+  }
+];
